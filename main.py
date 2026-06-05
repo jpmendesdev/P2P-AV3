b75a707ab45a123d813cccd74b88b46077edb8af
João Pedro <jotamendes944@gmail.com>
2026-06-05 14:28:18 -0300

    implementação de cache, informed flooding e informed walking

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 from network import P2PNetwork
 
 rede = P2PNetwork()
+
 rede.load_config("exemplo.yaml")
 
 print("Rede válida?", rede.validate())
 
-print("\nFlooding:")
-print(rede.flooding("n1", "r5", ttl=5))
+print("\nFlooding")
+print(
+    rede.search(
+        "n1",
+        "r5",
+        ttl=5,
+        algo="flooding"
+    )
+)
 
-print("\nRandom Walk:")
-print(rede.random_walk("n1", "r5", ttl=10))
+print("\nRandom Walk")
+print(
+    rede.search(
+        "n1",
+        "r5",
+        ttl=10,
+        algo="random_walk"
+    )
+)
+
+print("\nInformed Flooding (1ª busca)")
+print(
+    rede.search(
+        "n1",
+        "r5",
+        ttl=5,
+        algo="informed_flooding"
+    )
+)
+
+print("\nInformed Flooding (2ª busca)")
+print(
+    rede.search(
+        "n1",
+        "r5",
+        ttl=5,
+        algo="informed_flooding"
+    )
+)
+
+print("\nCache de n1")
+print(rede.nodes["n1"].cache)
--- network.py
+++ network.py
@@ -1,8 +1,8 @@
-
 from collections import deque
 import random
 import yaml
 from node import Node
+
 
 class P2PNetwork:
     def __init__(self):
@@ -20,6 +20,7 @@ class P2PNetwork:
     def add_edge(self, node1, node2):
         if node1 == node2:
             raise ValueError("Não são permitidos laços")
+
         self.nodes[node1].neighbors.add(node2)
         self.nodes[node2].neighbors.add(node1)
 
@@ -41,12 +42,17 @@ class P2PNetwork:
             self.add_edge(node1, node2)
 
     def is_connected(self):
+        if not self.nodes:
+            return True
+
         start = next(iter(self.nodes))
+
         visited = {start}
         queue = deque([start])
 
         while queue:
             current = queue.popleft()
+
             for neighbor in self.nodes[current].neighbors:
                 if neighbor not in visited:
                     visited.add(neighbor)
@@ -62,20 +68,28 @@ class P2PNetwork:
             degree = len(node.neighbors)
 
             if degree < self.min_neighbors:
-                raise ValueError(f"{node.id} possui poucos vizinhos")
+                raise ValueError(
+                    f"{node.id} possui poucos vizinhos"
+                )
 
             if degree > self.max_neighbors:
-                raise ValueError(f"{node.id} possui muitos vizinhos")
+                raise ValueError(
+                    f"{node.id} possui muitos vizinhos"
+                )
 
             if not node.resources:
-                raise ValueError(f"{node.id} não possui recursos")
+                raise ValueError(
+                    f"{node.id} não possui recursos"
+                )
 
         return True
 
     def flooding(self, start_node, resource_id, ttl):
         queue = deque([(start_node, ttl)])
+
         visited = set()
         involved = set()
+
         messages = 0
 
         while queue:
@@ -94,7 +108,8 @@ class P2PNetwork:
                     "found": True,
                     "owner": current,
                     "messages": messages,
-                    "nodes_involved": len(involved)
+                    "nodes_involved": len(involved),
+                    "visited_nodes": involved
                 }
 
             if current_ttl <= 0:
@@ -102,17 +117,21 @@ class P2PNetwork:
 
             for neighbor in node.neighbors:
                 messages += 1
-                queue.append((neighbor, current_ttl - 1))
+                queue.append(
+                    (neighbor, current_ttl - 1)
+                )
 
         return {
             "found": False,
             "owner": None,
             "messages": messages,
-            "nodes_involved": len(involved)
+            "nodes_involved": len(involved),
+            "visited_nodes": involved
         }
 
     def random_walk(self, start_node, resource_id, ttl):
         current = start_node
+
         involved = {current}
         messages = 0
 
@@ -124,14 +143,19 @@ class P2PNetwork:
                     "found": True,
                     "owner": current,
                     "messages": messages,
-                    "nodes_involved": len(involved)
+                    "nodes_involved": len(involved),
+                    "visited_nodes": involved
                 }
 
             if ttl == 0 or not node.neighbors:
                 break
 
-            current = random.choice(list(node.neighbors))
+            current = random.choice(
+                list(node.neighbors)
+            )
+
             involved.add(current)
+
             messages += 1
             ttl -= 1
 
@@ -139,5 +163,126 @@ class P2PNetwork:
             "found": False,
             "owner": None,
             "messages": messages,
-            "nodes_involved": len(involved)
+            "nodes_involved": len(involved),
+            "visited_nodes": involved
         }
+
+    def update_cache(
+        self,
+        involved_nodes,
+        resource_id,
+        owner
+    ):
+        for node_id in involved_nodes:
+            self.nodes[node_id].cache[
+                resource_id
+            ] = owner
+
+    def informed_flooding(
+        self,
+        start_node,
+        resource_id,
+        ttl
+    ):
+        start = self.nodes[start_node]
+
+        if resource_id in start.cache:
+            return {
+                "found": True,
+                "owner": start.cache[resource_id],
+                "messages": 0,
+                "nodes_involved": 1,
+                "visited_nodes": {start_node},
+                "cache_hit": True
+            }
+
+        result = self.flooding(
+            start_node,
+            resource_id,
+            ttl
+        )
+
+        if result["found"]:
+            self.update_cache(
+                result["visited_nodes"],
+                resource_id,
+                result["owner"]
+            )
+
+        result["cache_hit"] = False
+
+        return result
+
+    def informed_random_walk(
+        self,
+        start_node,
+        resource_id,
+        ttl
+    ):
+        start = self.nodes[start_node]
+
+        if resource_id in start.cache:
+            return {
+                "found": True,
+                "owner": start.cache[resource_id],
+                "messages": 0,
+                "nodes_involved": 1,
+                "visited_nodes": {start_node},
+                "cache_hit": True
+            }
+
+        result = self.random_walk(
+            start_node,
+            resource_id,
+            ttl
+        )
+
+        if result["found"]:
+            self.update_cache(
+                result["visited_nodes"],
+                resource_id,
+                result["owner"]
+            )
+
+        result["cache_hit"] = False
+
+        return result
+
+    def search(
+        self,
+        node_id,
+        resource_id,
+        ttl,
+        algo
+    ):
+        if algo == "flooding":
+            return self.flooding(
+                node_id,
+                resource_id,
+                ttl
+            )
+
+        elif algo == "random_walk":
+            return self.random_walk(
+                node_id,
+                resource_id,
+                ttl
+            )
+
+        elif algo == "informed_flooding":
+            return self.informed_flooding(
+                node_id,
+                resource_id,
+                ttl
+            )
+
+        elif algo == "informed_random_walk":
+            return self.informed_random_walk(
+                node_id,
+                resource_id,
+                ttl
+            )
+
+        raise ValueError(
+            f"Algoritmo inválido: {algo}"
+        )
